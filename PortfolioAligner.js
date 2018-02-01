e52aeee6c9e93d8ac0765eabe690186ed9bff298
x4iiiis <ryan_o_f@hotmail.co.uk>
2018-02-01 22:04:45 +0000

    PortfolioAligner FINALLY working :sweat_smile:

--- PortfolioAligner.js
+++ PortfolioAligner.js
@@ -1,27 +1,64 @@
-//var divs = document.getElementsByTagName("div");
-//for(var i = 0; i < divs.length; i++){
-   //do something to each div like
-//   divs[i].innerHTML = "something new...";
-//}
+alert("Hey");
+console.log("function starting at least...");
 
-var divs = document.getElementsByTagName("div");
-var divImages = divs[i].getElementsByTagName("img");
+
+var container = document.getElementById("container");
+var alldivs = container.getElementsByTagName("div");
+//var alldivs = document.getElementsByTagName("div");
+//var container = document.getElementsByID("container");
+var divsincontainer = [];
+var divs = [];
 var temp = "float-img-left";
+
+console.log(typeof(alldivs));
+console.log(typeof(alldivs[0]));
+console.log("divs: " + alldivs.length);
+    
+for(var i = 0; i < alldivs.length; i++)
+{
+    //if(container. (alldivs[i]));
+    //{
+    //    console.log("continuing...");
+    //    continue;
+    //}
+
+    console.log("container.length: " + container.length);
+
+    var divImages = alldivs[i].getElementsByTagName("img");
+
+    console.log("divImages.length: " + divImages.length);
+
+    //Remove divs with no images from divs
+    if(divImages.length != 0 && alldivs[i].className != "Section")
+    {
+        divs.push(alldivs[i]);
+        console.log("Pushing...");
+    }
+}
+
+console.log("updated divs: " + divs.length);
+
 for(var i = 0; i < divs.length; i++)
 {
-    //Make even numbers float-img-right
-    //Make odd numbers float-img-left
-    if(i % 2 == 0)
+    var divImages = divs[i].getElementsByTagName("img");
+
+    if(i%2 == 0)
     {
-        temp = "float-img-right";
+        temp = "float-img-left";      
+        console.log(i + "%2 == 0");  
     }
     else
     {
-        temp = "float-img-left";
+        temp = "float-img-right";     
+        console.log(i + "%2 != 0");     
     }
 
     for(var j = 0; j < divImages.length; j++)
     {
-        divImages[i].class=temp;
+        console.log("Right what the fuck? TEMP: " + temp);
+        console.log(divs[i].id);
+        divImages[j].classList.add(temp);
+        console.log(divImages[j].src);
+        console.log(divImages[j].classList);
     }
 }
