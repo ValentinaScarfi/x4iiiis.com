be1b0774a66486bcba04ce8c76d90d61d3fbc293
x4iiiis <ryan_o_f@hotmail.co.uk>
2018-02-01 22:37:53 +0000

    DK

--- PortfolioAligner.js
+++ PortfolioAligner.js
@@ -1,42 +1,21 @@
-alert("Hey");
-console.log("function starting at least...");
-
-
 var container = document.getElementById("container");
 var alldivs = container.getElementsByTagName("div");
-//var alldivs = document.getElementsByTagName("div");
-//var container = document.getElementsByID("container");
-var divsincontainer = [];
 var divs = [];
 var temp = "float-img-left";
-
-console.log(typeof(alldivs));
-console.log(typeof(alldivs[0]));
-console.log("divs: " + alldivs.length);
     
 for(var i = 0; i < alldivs.length; i++)
 {
-    //if(container. (alldivs[i]));
-    //{
-    //    console.log("continuing...");
-    //    continue;
-    //}
-
     console.log("container.length: " + container.length);
 
     var divImages = alldivs[i].getElementsByTagName("img");
 
     console.log("divImages.length: " + divImages.length);
 
-    //Remove divs with no images from divs
     if(divImages.length != 0 && alldivs[i].className != "Section")
     {
         divs.push(alldivs[i]);
-        console.log("Pushing...");
     }
 }
-
-console.log("updated divs: " + divs.length);
 
 for(var i = 0; i < divs.length; i++)
 {
@@ -44,21 +23,15 @@ for(var i = 0; i < divs.length; i++)
 
     if(i%2 == 0)
     {
-        temp = "float-img-left";      
-        console.log(i + "%2 == 0");  
+        temp = "float-img-right";
     }
     else
     {
-        temp = "float-img-right";     
-        console.log(i + "%2 != 0");     
+        temp = "float-img-left";
     }
 
     for(var j = 0; j < divImages.length; j++)
     {
-        console.log("Right what the fuck? TEMP: " + temp);
-        console.log(divs[i].id);
         divImages[j].classList.add(temp);
-        console.log(divImages[j].src);
-        console.log(divImages[j].classList);
     }
 }
